50e24753c5e49749d05f62075fe219e8b81dd1a3
Paweł Zalewski <pawelz@staszic.waw.pl>
2025-11-23 00:06:13 +0100

    add cargo dependenct, inital test

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,7 +1,6 @@
 use sqlite::Connection;
-use sqlite::State;
 
-use crate::PathBuf;
+use crate::{PathBuf, benchmarking};
 pub struct Database {
     connection: Connection,
 }
@@ -15,6 +14,22 @@ pub struct BenchmarkParams {
 
 pub struct BenchmarkResult {
     data_json: Option<String>,
+}
+
+impl BenchmarkParams {
+    pub fn new(
+        commit_hash: String,
+        benchmark_type: String,
+        benchmark_argument: u64,
+        measurement_method: String,
+    ) -> BenchmarkParams {
+        BenchmarkParams {
+            commit_hash,
+            benchmark_type,
+            benchmark_argument,
+            measurement_method,
+        }
+    }
 }
 
 impl BenchmarkResult {
@@ -95,11 +110,8 @@ impl Database {
         stmt.bind((4, params.measurement_method.as_str()))?;
 
         stmt.next()?;
-        let data_json: String = stmt.read(0)?;
 
-        Ok(Some(BenchmarkResult {
-            data_json: Some(data_json),
-        }))
+        Ok(Some(BenchmarkResult::new(stmt.read(0)?)))
     }
 
     pub fn data_exists(&self, params: BenchmarkParams) -> Result<bool, sqlite::Error> {
@@ -108,6 +120,35 @@ impl Database {
 }
 
 mod tests {
+    use std::path::PathBuf;
+    use uuid::Uuid;
+    use std::fs;
+
+    fn tmp_db_path() -> PathBuf {
+        let filename = format!("test_db_{}.sqlite", Uuid::new_v4());
+        PathBuf::from(filename)
+    }
+
+    fn cleanup(path: &PathBuf) {
+        let _ = fs::remove_file(path);
+    }
+
     #[test]
-    fn test() {}
+    fn insert_get() {
+
+    }
+
+    #[test]
+    fn get_doesnt_exist(){
+
+    }
+
+    #[test]
+    fn double_insert(){
+
+    }
+
+    fn timeout_test(){
+
+    }
 }
