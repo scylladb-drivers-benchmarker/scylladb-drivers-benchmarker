b2cb9e183963c3eaa0dc172236260c0413b18b8f
Paweł Mieszkowski <cs.pawelmieszkowski@gmail.com>
2025-11-25 09:52:35 +0100

    refactor

--- src/benchmarking/mod.rs
+++ src/benchmarking/mod.rs
@@ -1,19 +1,11 @@
 mod execution;
 
-use std::{
-    error::Error,
-    iter::zip,
-    path::PathBuf,
-};
+use std::{error::Error, iter::zip, path::PathBuf};
 
-use crate::commit_hash::CommitHash;
+use crate::{commit_hash::CommitHash, database};
 use execution::{Executor, SourceCode, compile};
 
-use crate::config::{
-    backend::BackendConfig,
-    benchmark::BenchmarkConfig,
-    find_config,
-};
+use crate::config::{backend::BackendConfig, benchmark::BenchmarkConfig, find_config};
 
 use super::database::*;
 
@@ -38,11 +30,13 @@ pub struct BenchmarkingArguments {
     pub instrumentation: String,
 }
 
-fn benchmark(
+type ErrBenchmarkResult = Result<database::BenchmarkResult, Box<dyn Error>>;
+
+fn benchmark_all(
     args: &BenchmarkingArguments,
     benchmark_config: &BenchmarkConfig,
     backend_config: &BackendConfig,
-) -> Result<Vec<String>, Box<dyn Error>> {
+) -> Result<impl Iterator<Item = ErrBenchmarkResult>, Box<dyn Error>> {
     let compiled_source = compile(
         backend_config.build_command.as_str(),
         SourceCode { path: None },
@@ -51,34 +45,37 @@ fn benchmark(
     let mut executor = Executor::new(compiled_source, backend_config.run_command.as_str())?;
     executor.measure(args.instrumentation.as_ref())?;
 
-    let mut outputs: Vec<String> = Vec::new();
-    for benchmark_point in benchmark_config.iter_points() {
+    let benchmark_one = move |benchmark_point: u32| -> ErrBenchmarkResult {
         let output = executor.execute(benchmark_point)?;
         let output = String::from_utf8(output.stdout)?;
-        outputs.push(output);
-    }
+        Ok(database::BenchmarkResult::new(Some(output)))
+    };
 
-    Ok(outputs)
+    Ok(benchmark_config.iter_points().map(benchmark_one))
 }
 
 fn save_benchmark_results(
     args: &BenchmarkingArguments,
     git_hash: CommitHash,
-    results: Vec<String>,
+    results: impl Iterator<Item = ErrBenchmarkResult>,
     benchmark_config: &BenchmarkConfig,
-    backend_config: &BackendConfig,
     database: &Database,
 ) -> Result<(), Box<dyn Error>> {
     for (param, result) in zip(benchmark_config.iter_points(), results) {
-        database.insert_data(
-            BenchmarkParams::new(
-                git_hash.clone(),
-                args.benchmark_name.clone(),
-                param.into(),
-                args.instrumentation.clone(),
-            ),
-            BenchmarkResult::new(Some(result)),
-        )?
+        match result {
+            Err(error) => {
+                return Err(error);
+            }
+            Ok(correct) => database.insert_data(
+                BenchmarkParams::new(
+                    git_hash.clone(),
+                    args.benchmark_name.clone(),
+                    param.into(),
+                    args.instrumentation.clone(),
+                ),
+                correct,
+            )?,
+        }
     }
 
     Ok(())
@@ -89,14 +86,7 @@ fn main(args: &BenchmarkingArguments, database: &Database) -> Result<(), Box<dyn
         find_config(&args.benchmark_name, &args.driver_config_path)?;
     let backend_config: BackendConfig = find_config(&args.driver_name, &args.driver_config_path)?;
 
-    let git_hash = CommitHash::from_repository()?;
-    let results = benchmark(args, &benchmark_config, &backend_config)?;
-    save_benchmark_results(
-        args,
-        git_hash,
-        results,
-        &benchmark_config,
-        &backend_config,
-        database,
-    )
+    let git_hash: CommitHash = CommitHash::from_repository()?;
+    let results = benchmark_all(args, &benchmark_config, &backend_config)?;
+    save_benchmark_results(args, git_hash, results, &benchmark_config, database)
 }
