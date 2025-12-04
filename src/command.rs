f5674fe9e794cca95fe11f7761d7266ff9cc36ed
Paweł Mieszkowski <cs.pawelmieszkowski@gmail.com>
2025-12-04 22:26:26 +0100

    thiserror in command

--- src/command.rs
+++ src/command.rs
@@ -1,7 +1,6 @@
 use core::fmt;
 use std::str::FromStr;
-
-use shlex::Shlex;
+use thiserror::Error;
 
 /// Struct used for parsing command from configs
 #[derive(Debug, Clone)]
@@ -22,28 +21,26 @@ impl fmt::Display for Command {
     }
 }
 
+#[derive(Error, Debug, PartialEq, Eq)]
+pub enum CommandParsingError {
+    #[error("program not given")]
+    ProgramNotGiven,
+    #[error("failed parsing <- {0}")]
+    FailedShlexing( #[from] #[source] shell_words::ParseError),
+}
+
 impl FromStr for Command {
-    type Err = String;
+    type Err = CommandParsingError;
 
     fn from_str(command: &str) -> Result<Self, Self::Err> {
-        let mut it = Shlex::new(command);
-        let program: String = it.next().ok_or("Program name not given")?;
-        if it.had_error {
-            return Err("Failed parsing program name.".to_string());
-        }
-
-        let mut arguments: Vec<String> = Vec::new();
-        for next_value in it.by_ref() {
-            arguments.push(next_value);
-        }
-
-        if it.had_error {
-            let erroneous = arguments
-                .last()
-                .expect("Shlex should first mark had_error before stopping");
-            Err(String::from("Wrong argument: ") + erroneous)
+        let mut words = shell_words::split(command)?;
+        if words.is_empty() {
+            Err(CommandParsingError::ProgramNotGiven)
         } else {
-            Ok(Command { program, arguments })
+            Ok(Command {
+                program: words.remove(0),
+                arguments: words,
+            })
         }
     }
 }
@@ -105,8 +102,8 @@ mod test {
     use super::*;
 
     #[test]
-    fn command_from_str() -> Result<(), Box<dyn Error>> {
-        let mut command = Command::from_str("git commit -m \"This is a commit message\"")?;
+    fn command_from_str() {
+        let mut command = Command::from_str("git commit -m \"This is a commit message\"").unwrap();
         command.add_argument("--author".to_owned());
         command.add_argument("This is a commit author".to_owned());
         let process_cmd = command.process();
@@ -117,7 +114,24 @@ mod test {
         assert_eq!(args[2], "This is a commit message");
         assert_eq!(args[3], "--author");
         assert_eq!(args[4], "This is a commit author");
-        Ok(())
+    }
+
+    #[test]
+    fn from_str_error() {
+        assert_eq!(
+            Command::from_str("").unwrap_err(),
+            CommandParsingError::ProgramNotGiven
+        );
+
+        assert_eq!(
+            Command::from_str("\"unfinished quoting").unwrap_err(),
+            CommandParsingError::FailedShlexing(shell_words::ParseError)
+        );
+
+        assert_eq!(
+            Command::from_str("git -m \"unfinished quoting").unwrap_err(),
+            CommandParsingError::FailedShlexing(shell_words::ParseError)
+        );
     }
 
     #[test]
