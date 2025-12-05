8c9124f90a98e7030b828b27edb7ada35ff23d00
MakuwaDev <github@rob.pl>
2025-12-05 20:10:36 +0100

    refactor plotting

---
+++ src/plotting/result.rs
@@ -0,0 +1,81 @@
+use crate::utilities::BenchmarkPoint;
+use plotters::prelude::*;
+use plotters::coord::types::{RangedCoordu64, RangedCoordf64};
+
+pub(crate) trait PlottableResult<'a, BC> {
+    fn add_to_plot(&self, backend_or_chart: &mut BC) -> Result<(), Box<dyn std::error::Error>>;
+}
+
+pub(crate) struct PlottableSeries {
+    pub name: String,
+    pub points: Vec<BenchmarkPoint>,
+    pub series: Vec<Option<f64>>,
+    pub color: PaletteColor<Palette99>,
+    pub range: Option<(f64, f64)>,
+}
+
+impl PlottableSeries {
+    pub(crate) fn new(
+        name: String,
+        points: Vec<BenchmarkPoint>,
+        series: Vec<Option<f64>>,
+        color: PaletteColor<Palette99>,
+        range: Option<(f64, f64)>,
+    ) -> Self {
+        PlottableSeries {
+            name,
+            points,
+            series,
+            color,
+            range,
+        }
+    }
+
+    pub fn range(&self) -> Option<(f64, f64)> {
+        self.range
+    }
+}
+
+impl<'a>
+    PlottableResult<
+        'a,
+        ChartContext<'a, BitMapBackend<'a>, Cartesian2d<RangedCoordu64, RangedCoordf64>>,
+    > for PlottableSeries
+{
+    fn add_to_plot(
+        &self,
+        chart: &mut ChartContext<
+            'a,
+            BitMapBackend<'a>,
+            Cartesian2d<RangedCoordu64, RangedCoordf64>,
+        >,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let color = self.color.to_rgba();
+        let name = self.name.clone();
+        let y_max = chart.as_coord_spec().y_spec().range().end;
+
+        let mut line_points: Vec<(BenchmarkPoint, f64)> = Vec::new();
+
+        for (&x, y_opt) in self.points.iter().zip(self.series.iter()) {
+            match y_opt {
+                Some(y) => line_points.push((x, *y)),
+                None => {
+                    chart.draw_series(std::iter::once(Cross::new((x, y_max / 2.0), 5, color)))?;
+                }
+            }
+        }
+
+        chart
+            .draw_series(LineSeries::new(line_points, color))?
+            .label(name)
+            .legend(move |(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], color));
+
+        chart
+            .configure_series_labels()
+            .border_style(BLACK)
+            .background_style(WHITE.mix(0.8))
+            .draw()?;
+
+        Ok(())
+    }
+}
---
+++ src/plotting/series.rs
@@ -0,0 +1,82 @@
+use super::data::PlottableValue;
+
+pub(crate) enum VisKind {
+    Linear,
+    Log,
+}
+
+pub(crate) trait SeriesValue: PlottableValue + PartialOrd + Into<f64> {}
+impl<T: PlottableValue + PartialOrd + Into<f64>> SeriesValue for T {}
+
+pub(crate) struct LinearSeries<T: SeriesValue> {
+    pub y: Vec<Option<T>>,
+}
+
+pub(crate) struct LogSeries<T: SeriesValue> {
+    pub y: Vec<Option<T>>,
+}
+
+
+pub(crate) enum SeriesKind<T: SeriesValue> {
+    Linear(LinearSeries<T>),
+    Log(LogSeries<T>),
+}
+
+fn calc_range<T: SeriesValue>(iter: impl Iterator<Item = T>) -> Option<(T, T)> {
+    let mut iter = iter;
+    let first = iter.next()?;
+    let (mut min, mut max) = (first.clone(), first);
+
+    for v in iter {
+        if v < min { min = v.clone() }
+        if v > max { max = v.clone() }
+    }
+
+    Some((min, max))
+}
+
+impl<T: SeriesValue> LinearSeries<T> {
+    fn series(&self) -> Vec<Option<f64>> {
+        self.y
+            .iter()
+            .cloned()
+            .map(|y| y.map(|v| v.into()))
+            .collect()
+    }
+
+    fn range(&self) -> Option<(f64, f64)> {
+        let (min, max) = calc_range(self.y.iter().filter_map(|v| v.clone()))?;
+        Some((min.into(), max.into()))
+    }
+}
+
+impl<T: SeriesValue> LogSeries<T> {
+    fn series(&self) -> Vec<Option<f64>> {
+        self.y
+            .iter()
+            .cloned()
+            .map(|y| y.map(|v| v.into().log10()))
+            .collect()
+    }
+
+    fn range(&self) -> Option<(f64, f64)> {
+        let (min, max) = calc_range(self.y.iter().filter_map(|v| v.clone()))?;
+        Some((min.into().log10(), max.into().log10()))
+    }
+}
+
+impl<T: SeriesValue> SeriesKind<T> {
+    pub fn series(&self) -> Vec<Option<f64>> {
+        match self {
+            SeriesKind::Linear(s) => s.series(),
+            SeriesKind::Log(s) => s.series(),
+        }
+    }
+
+    pub fn range(&self) -> Option<(f64, f64)> {
+        match self {
+            SeriesKind::Linear(s) => s.range(),
+            SeriesKind::Log(s) => s.range(),
+        }
+    }
+}
