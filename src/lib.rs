d0e04fef8f8f6b97c4e207e6f600937d1c23f0e4
Paweł Zalewski <pawelz@staszic.waw.pl>
2025-12-07 00:51:57 +0100

    merge

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@ pub fn run_benchmarks(
 ) -> Result<(), Box<dyn Error>> {
     let benchmark_config: BenchmarkConfig = find_config(benchmark_name, benchmark_config_path)?;
 
-    let commit_hash: CommitHash = CommitHash::from_repository()?;
+    let commit_hash: CommitHash = CommitHash::from_current_repository()?;
     benchmarking::benchmark(
         database,
         commit_hash,
