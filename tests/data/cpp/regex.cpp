ea203693d09b250b0d05526b8a8c860047bce2da
Paweł Mieszkowski <cs.pawelmieszkowski@gmail.com>
2025-12-07 02:46:52 +0100

    small cpp test fixes

--- tests/data/cpp/regex.cpp
+++ tests/data/cpp/regex.cpp
@@ -3,13 +3,14 @@
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cout << "Usage: " << argv[0] << " <N>";
+        return -1;
     }
 
     size_t size;
     std::stringstream{argv[1]} >> size;
     std::string str(size, 'a');
 
-    std::regex regex{"^[a,b]*$"}; // string consists of only a and b
+    std::regex regex{"^[ab]*$"}; // string consists of only a and b
     
     std::cout << std::regex_match(str, regex) << '\n';
 }
