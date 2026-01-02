37c9c3ba8e145783d2ef151ed35e3a63daa3dead
Paweł Mieszkowski <cs.pawelmieszkowski@gmail.com>
2026-01-02 17:34:32 +0100

    restructured integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 use assert_cmd::cargo;
 use scylladb_drivers_benchmarker::{
     commit_hash::CommitHash,
-    database::{self},
+    database::{self, Database},
     utilities::{BenchmarkParams, BenchmarkRecord},
 };
 
@@ -44,32 +44,6 @@ fn setup_git(repo: &str) {
     );
 }
 
-fn gather_data(path: &str) -> CommitHash {
-    let mut command = std::process::Command::new(cargo::cargo_bin!("scylladb-drivers-benchmarker"));
-    command
-        .current_dir(path)
-        .arg("-d")
-        .arg("../test.db")
-        .arg("-b")
-        .arg("../config.yml")
-        .arg("regex")
-        .arg("run");
-    let output = command.output().unwrap();
-    if !output.status.success() {
-        println!(
-            "my_stdout: {}",
-            String::from_utf8_lossy(output.stdout.as_slice())
-        );
-        println!(
-            "my_stderr: {}",
-            String::from_utf8_lossy(output.stderr.as_slice())
-        );
-        println!("{}", path);
-        assert!(output.status.success());
-    }
-    CommitHash::new(Path::new(path), "HEAD".to_owned()).unwrap()
-}
-
 fn check_data(
     commit_hash: &CommitHash,
     data: impl Iterator<Item = (BenchmarkParams, BenchmarkRecord)>,
@@ -94,29 +68,68 @@ fn check_data(
     assert_eq!(cnt, 8usize);
 }
 
-#[test]
-fn test_cpp_vs_rust() {
-    setup_git("./tests/data/cpp/");
-    setup_git("./tests/data/rust/");
+struct CppVsRust {
+    db: Database,
+}
 
+impl CppVsRust {
+    fn new(db: database::Database) -> Self {
+        setup_git("./tests/data/cpp/");
+        setup_git("./tests/data/rust/");
+        CppVsRust { db }
+    }
+
+    fn gather_data(&self, path: &str, command: &mut std::process::Command) -> CommitHash {
+        let output = command.current_dir(path).output().unwrap();
+        if !output.status.success() {
+            println!(
+                "my_stdout: {}",
+                String::from_utf8_lossy(output.stdout.as_slice())
+            );
+            println!(
+                "my_stderr: {}",
+                String::from_utf8_lossy(output.stderr.as_slice())
+            );
+            println!("{}", path);
+            assert!(output.status.success());
+        }
+        CommitHash::new(Path::new(path), "HEAD".to_owned()).unwrap()
+    }
+
+    fn run(&self, command: &mut std::process::Command) {
+        let hash_cpp = self.gather_data("./tests/data/cpp/", command);
+        let hash_rust = self.gather_data("./tests/data/rust/", command);
+        assert!(hash_cpp != hash_rust);
+
+        let db_data = self.db.get_all_data().unwrap();
+
+        let data_cpp = db_data
+            .iter()
+            .filter(|(params, _)| params.commit_hash == hash_cpp)
+            .map(Clone::clone);
+        check_data(&hash_cpp, data_cpp);
+
+        let data_rust = db_data
+            .iter()
+            .filter(|(params, _)| params.commit_hash == hash_rust)
+            .map(Clone::clone);
+        check_data(&hash_rust, data_rust);
+    }
+}
+
+#[test]
+fn simple() {
     let db = database::Database::new(Path::new("./tests/data/test.db").to_owned()).unwrap();
     db.drop_all_data().unwrap();
 
-    let hash_cpp = gather_data("./tests/data/cpp/");
-    let hash_rust = gather_data("./tests/data/rust/");
-    assert!(hash_cpp != hash_rust);
+    let mut command = std::process::Command::new(cargo::cargo_bin!("scylladb-drivers-benchmarker"));
+    command
+        .arg("-d")
+        .arg("../test.db")
+        .arg("-b")
+        .arg("../config.yml")
+        .arg("regex")
+        .arg("run");
 
-    let db_data = db.get_all_data().unwrap();
-
-    let data_cpp = db_data
-        .iter()
-        .filter(|(params, _)| params.commit_hash == hash_cpp)
-        .map(Clone::clone);
-    check_data(&hash_cpp, data_cpp);
-
-    let data_rust = db_data
-        .iter()
-        .filter(|(params, _)| params.commit_hash == hash_rust)
-        .map(Clone::clone);
-    check_data(&hash_rust, data_rust);
+    CppVsRust::new(db).run(&mut command);
 }
