5c5d497af8e5d683a0d5417ba7005debd94356fc
Paweł Zalewski <pawelz@staszic.waw.pl>
2026-01-04 12:36:28 +0100

    nvm

--- tests/benchmark_test.rs
+++ tests/benchmark_test.rs
@@ -133,7 +133,6 @@ fn sdb_command() -> std::process::Command {
 // We chose to serialize those two tests to avoid unpredictable test failures.
 
 #[test]
-// TODO explain
 #[file_serial]
 fn simple() {
     let mut command = sdb_command();
