eba12d60f577d3b7ed8c43c50ad1ab26dfcd8710
Krzysztof Hałubek <github@rob.pl>
2026-01-09 19:39:57 +0100

    typo fix

--- tests/common.rs
+++ tests/common.rs
@@ -11,14 +11,14 @@ pub fn run(cmd: &mut std::process::Command) -> Output {
     run_safe(cmd, |output: &Output| output.status.success())
 }
 
-pub fn run_safe(cmd: &mut std::process::Command, werifier: impl Fn(&Output) -> bool) -> Output {
+pub fn run_safe(cmd: &mut std::process::Command, verifier: impl Fn(&Output) -> bool) -> Output {
     let wrong_output = format!(
         "Command: \"{}\" failed",
         command::Command::from_command_lossy(cmd)
     );
 
     let output = cmd.output().expect(&wrong_output);
-    if !werifier(&output) {
+    if !verifier(&output) {
         println!("{}", wrong_output);
         println!("stdout:\n{}", String::from_utf8_lossy(&output.stdout));
         println!("stderr:\n{}", String::from_utf8_lossy(&output.stderr));
