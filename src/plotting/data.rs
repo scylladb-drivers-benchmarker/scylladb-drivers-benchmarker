e7f55e5ac400bc69f1e6f5cf3b16663b3072e8e9
Krzysztof Hałubek <github@rob.pl>
2026-01-09 17:01:18 +0100

    db trailing newline fix

--- src/plotting/data.rs
+++ src/plotting/data.rs
@@ -73,10 +73,8 @@ impl<T: PlottableValue> BenchmarkDataset<T> {
 
             match database.get_result(params)?.map(|r| r.flatten()) {
                 Some(FlatBenchmarkRecord::Data(text)) => {
-                    // TODO this will be in a diff place
-                    let cleaned = text.trim_end_matches('\n').to_string();
                     let value =
-                        T::from_str(&cleaned).map_err(|_| PlotError::InvalidData(cleaned.clone()))?;
+                        T::from_str(&text).map_err(|_| PlotError::InvalidData(text.clone()))?;
                     results.push(Some(value));
                 }
                 Some(FlatBenchmarkRecord::Timeout) => results.push(None),
