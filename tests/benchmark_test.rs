8ce2f4dda8769ee5d672d32741694bf4a87a00a5
Paweł Mieszkowski <cs.pawelmieszkowski@gmail.com>
2026-01-09 17:17:22 +0100

    cpp vs rust fix

--- tests/benchmark_test.rs
+++ tests/benchmark_test.rs
@@ -23,6 +23,11 @@ fn setup_git(repo: &str) {
         .current_dir(repo)
         .arg("add")
         .arg("-A"));
+    run(std::process::Command::new("git")
+        .current_dir(repo)
+        .arg("commit")
+        .arg("-m")
+        .arg("\"initial\""));
 }
 
 fn check_data(
