00c84163bd8a2da60480997950354175e0d315c7
Paweł Mieszkowski <cs.pawelmieszkowski@gmail.com>
2026-01-11 20:47:02 +0100

    cargo clippy bullshit

--- src/benchmarking/executor/flame_executor.rs
+++ src/benchmarking/executor/flame_executor.rs
@@ -51,7 +51,7 @@ impl FlameExecutor {
 
     fn collect_output(pair: (Option<Vec<u8>>, Option<Vec<u8>>)) -> (Vec<u8>, Vec<u8>) {
         let (stdout, stderr) = pair;
-        return (stdout.unwrap(), stderr.unwrap());
+        (stdout.unwrap(), stderr.unwrap())
     }
 }
 
