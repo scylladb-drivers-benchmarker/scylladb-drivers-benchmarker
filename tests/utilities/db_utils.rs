3245db42653b25cf07605949e75b35a4b1f0141f
Paweł Mieszkowski <cs.pawelmieszkowski@gmail.com>
2026-01-11 23:27:57 +0100

    small style

--- tests/utilities/db_utils.rs
+++ tests/utilities/db_utils.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 use scylladb_drivers_benchmarker::database::Database;
 
 pub fn open_clean_db(path: &Path) -> Database {
-    let db = Database::new(path).unwrap();
+    let db = Database::new(path).expect(&format!("Couldn't open at path: {}", path.display()));
     db.drop_all_data().unwrap();
     return db;
 }
